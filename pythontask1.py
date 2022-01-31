126bdcc4a1218bf3ca64c0952d127412f58429b8
Itz-Mario <mario.pire@hotmail.co.uk>
2022-01-31 12:19:07 +0000

    attempting tasks

---
+++ pythontask1.py
@@ -0,0 +1,9 @@
+print("Hello World")
+
+print("Hello Mario Pire")
+
+number1 = float(input("Please enter the first number: "))
+number2 = float(input("please enter the second number: "))
+answer = number1 + number2
+
+print(number1, "+", number2, "=", answer)
