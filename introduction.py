21c0f004384dc292d408c19b2cfd8f46ba94f66c
Itz-Mario <mario.pire@hotmail.co.uk>
2022-01-31 12:19:29 +0000

    attempting tasks

---
+++ introduction.py
@@ -0,0 +1,14 @@
+
+var1 = "01010110001" # String
+var1a = '01010110001' # String
+var2 = 1010110001    # Integer
+var3 = 1010110001.0  # Float
+var4 = True
+var4a = False 
+
+print(var2)
+print(var1 + var1a)
+print(var2 + var3)
+print("this is the output of two data types: ", var2 , var3)
+
+print("Hello World")
