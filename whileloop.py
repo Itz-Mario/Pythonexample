e51bfe10550f9bf60ceab6570cc257b189b869db
Itz-Mario <mario.pire@hotmail.co.uk>
2022-02-02 10:10:28 +0000

    commit

---
+++ whileloop.py
@@ -0,0 +1,16 @@
+
+
+
+#var = int(input("NUM: "))
+
+#while var > 0 and var < 2000:
+    #if var%3 == 0:
+        #print(var)
+    #var = var - 1
+
+countvar = 5
+
+while countvar > 0:
+        namevar1 = input("Type NAME: ")
+        print(f"{namevar1} is awesome!")
+        countvar = countvar - 1
