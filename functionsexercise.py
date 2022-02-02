1dde901749f4f143a806991b9244fa2c73577315
Itz-Mario <mario.pire@hotmail.co.uk>
2022-02-02 13:53:57 +0000

    commit

--- functionsexercise.py
+++ functionsexercise.py
@@ -1,9 +1,9 @@
-def ICT_Grade(homework_score, assessment_score, final_exam_score):
+def IT_Grade(homework_score, assessment_score, final_exam_score):
     answer = (homework_score*assessment_score*final_exam_score)*100
-    return "Your ICT grade is: ", float(answer)
+    return "Your IT grade is: ", float(answer)
 
 
-added_number = total_score = ICT_Grade
+added_number = total_score = IT_Grade
 print(added_number)
 
 
@@ -12,6 +12,6 @@ homework_score = int(input("Enter homework score /25: "))/25
 assessment_score = int(input("Enter assessment score /50: "))/50
 final_exam_score = int(input("Enter final exam score /100: "))/100
 
-var1 = ICT_Grade(homework_score, assessment_score, final_exam_score)
-print(f"Your ICT grade is:  {var1}")
+var1 = IT_Grade(homework_score, assessment_score, final_exam_score)
+print(f"Your IT grade is:  {var1}")
 
